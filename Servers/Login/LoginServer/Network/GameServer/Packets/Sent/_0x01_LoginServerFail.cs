b86639090c6f81a7dac5c76616d6944d430a08ed
nihrom <nihrom23@gmail.com>
2025-03-16 12:07:20 +0300

    LoginServer.Network.GameServer.Packets.Sent теперь создаёт пакеты классы

--- Servers/Login/LoginServer/Network/GameServer/Packets/Sent/_0x01_LoginServerFail.cs
+++ Servers/Login/LoginServer/Network/GameServer/Packets/Sent/_0x01_LoginServerFail.cs
@@ -6,6 +6,14 @@ public class _0x01_LoginServerFail : Packet
 {
     public _0x01_LoginServerFail(byte reason) : base(0x01)
     {
+        // public static final int REASON_IP_BANNED = 1;
+        // public static final int REASON_IP_RESERVED = 2;
+        // public static final int REASON_WRONG_HEXID = 3;
+        // public static final int REASON_ID_RESERVED = 4;
+        // public static final int REASON_NO_FREE_ID = 5;
+        // public static final int NOT_AUTHED = 6;
+        // public static final int REASON_ALREADY_LOGGED8IN = 7;
+        
         WriteByte(reason);
     }
 }
--- Servers/Login/LoginServer/Network/GameServer/ServersNetwork/PacketBuilder.cs
+++ Servers/Login/LoginServer/Network/GameServer/ServersNetwork/PacketBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using Common.Network;
+using LoginServer.Network.GameServer.Packets.Sent;
 
 namespace LoginServer.Network.GameServer.ServersNetwork;
 
@@ -6,71 +7,36 @@ public static class PacketBuilder
 {
     public static Packet InitLS_0x00(byte[] publicKey) //+
     {
-        var p = new Packet(0x00);
-        p.WriteInt(0x0106);
-        p.WriteInt(publicKey.Length);
-        p.WriteByte(publicKey);
-
-        return p;
+        return new _0x00_InitLS(publicKey);
     }
 
-    public static Packet LoginServerFail_0x01(int reason) //-
+    public static Packet LoginServerFail_0x01(byte reason) //-
     {
-        // public static final int REASON_IP_BANNED = 1;
-        // public static final int REASON_IP_RESERVED = 2;
-        // public static final int REASON_WRONG_HEXID = 3;
-        // public static final int REASON_ID_RESERVED = 4;
-        // public static final int REASON_NO_FREE_ID = 5;
-        // public static final int NOT_AUTHED = 6;
-        // public static final int REASON_ALREADY_LOGGED8IN = 7;
-        
-        var p = new Packet(0x01);
-        p.WriteInt(reason);
-
-        return p;
+        return new _0x01_LoginServerFail(reason);
     }
 
     public static Packet AuthResponse_0x02(byte serverId, string serverName) //+
     {
-        var p = new Packet(0x02);
-        p.WriteByte(serverId);
-        p.WriteString(serverName);
-
-        return p;
+        return new _0x02_AuthResponse(serverId,serverName );
     }
 
     public static Packet PlayerAuthResponse_0x03(string account, bool response) // +
     {
-        var p = new Packet(0x03);
-        p.WriteString(account);
-        p.WriteByte(response ? (byte)1 : (byte)0);
-        
-        return p;
+        return new _0x03_PlayerAuthResponse(account, response);
     }
 
     public static Packet KickPlayer_0x04(string account) // +
     {
-        var p = new Packet(0x04);
-        p.WriteString(account);
-
-        return p;
+        return new _0x04_KickPlayer(account);
     }
 
     public static Packet RequestCharacters_0x05(string account) // +
     {
-        var p = new Packet(0x05);
-        p.WriteString(account);
-
-        return p;
+        return new _0x05_RequestCharacters(account);
     }
 
     public static Packet ChangePasswordResponse_0x06(byte successful, string characterName, string msgToSend) // +
     {
-        var p = new Packet(0x06);
-        // writeByte(successful); // 0 false, 1 true //TODO: Разобраться со стороны гейм сервера надо ли. Неизвестно почему не отправляется
-        p.WriteString(characterName);
-        p.WriteString(msgToSend);
-
-        return p;
+        return new _0x06_ChangePasswordResponse(successful, characterName, msgToSend);
     }
 }
